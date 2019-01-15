54c59ed5405dc014a39512530e4be16cd650fd2b
Adam Radocz <adam.radocz@gmail.com>
2019-01-15 11:55:58 +0100

    Move IFilterableViewModel to ViewModels folder.

---
+++ BoincManagerWindows/ViewModels/IFilterableViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BoincManagerWindows.ViewModels
+{
+    interface IFilterableViewModel
+    {
+        IEnumerable<string> GetContentsForFiltering();
+    }
+}
