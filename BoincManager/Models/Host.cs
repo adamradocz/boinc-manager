b7521c9d5b509074b642d521ae040c3a5674dc84
Adam Radocz <adam.radocz@gmail.com>
2019-05-19 08:51:57 +0200

    Add data annotations to Host model.

--- BoincManager/Models/Host.cs
+++ BoincManager/Models/Host.cs
@@ -1,11 +1,22 @@
-﻿namespace BoincManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoincManager.Models
 {
     public class Host
     {
         public int Id { get; set; }
+        
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string IpAddress { get; set; }
+
+        [Required]
+        [Range(0, 65535)]
         public int Port { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }
